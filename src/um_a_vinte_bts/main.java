c1955fb1eec699614187fcf9733071ffa62fc407
DESKTOP-4A7G64Q\User <user@etec.com>
2026-08-18 18:42:10 -0300

    adicionados arquivos do repo

---
+++ src/um_a_vinte_bts/main.java
@@ -0,0 +1,18 @@
+package um_a_vinte_bts;
+
+import javax.swing.JOptionPane;
+
+public class main {
+
+	public static void main(String[] args) {
+			int i = 1;
+			while (i <= 20) {
+				int a = 0 + i;
+				JOptionPane.showMessageDialog(null, a);
+			    i++;
+			}
+
+
+	}
+
+}
